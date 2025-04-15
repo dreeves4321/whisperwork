42344f6015a6c7e90822d272e6c2e4d6c9864d25
Daniel Reeves <daniel@goinvo.com>
2025-04-15 09:24:40 -0400

    mobile styles almost done

--- src/components/ThreadFilter.tsx
+++ src/components/ThreadFilter.tsx
@@ -41,6 +41,36 @@ const ThreadFilter: React.FC<ThreadFilterProps> = ({
       <hr />
       <div className="thread-filter__flex">
         <div className="thread-filter__buttons">
+          <button
+            className="thread-filter__button--nav"
+            onClick={() => {
+              if (!activeThread) {
+                onThreadSelect(threads[0].id);
+                return;
+              }
+              const currentIndex = threads.findIndex(t => t.id === activeThread);
+              const prevIndex = currentIndex <= 0 ? threads.length - 1 : currentIndex - 1;
+              onThreadSelect(threads[prevIndex].id);
+            }}
+          >
+            <img src={`${process.env.PUBLIC_URL}/icons/arrow-left.svg`} alt="Previous" />
+          </button>
+          <div className="thread-filter__small--label">{threads.find(t => t.id === activeThread)?.name}</div>
+          <button
+            className="thread-filter__button--nav"
+            onClick={() => {
+              if (!activeThread) {
+                onThreadSelect(threads[0].id);
+                return;
+              }
+              const currentIndex = threads.findIndex(t => t.id === activeThread);
+              const nextIndex = currentIndex >= threads.length - 1 ? 0 : currentIndex + 1;
+              onThreadSelect(threads[nextIndex].id);
+            }}
+          >
+            <img src={`${process.env.PUBLIC_URL}/icons/arrow-right.svg`} alt="Next" />
+          </button>
+
           {threads.map((thread) => (
             <button
               key={thread.id}
@@ -49,14 +79,19 @@ const ThreadFilter: React.FC<ThreadFilterProps> = ({
               }`}
               onClick={() => onThreadSelect(thread.id)}
             >
-              <img src={`${process.env.PUBLIC_URL}/icons/star.svg`} alt=""  />
+              <img src={`${process.env.PUBLIC_URL}/icons/star.svg`} alt="" />
               {thread.name}
             </button>
           ))}
+
+          
         </div>
         <div className="thread-filter__content">          
         {threadDescription && (
-            <p className="thread-filter__description">{threadDescription}</p>
+            <p className="thread-filter__description">
+              <span className="thread-filter__name">{threads.find(t => t.id === activeThread)?.name} - </span>
+              {threadDescription}
+            </p>
           )}
           {featuredItem && (
             <div className="thread-filter__featured">
